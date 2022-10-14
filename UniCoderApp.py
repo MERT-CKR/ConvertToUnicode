b3dbed98aef87c9998b64d8d518563fbb017da3f
Luvica <56768mert@gmail.com>
2022-10-14 20:31:07 +0300

    gönderi

---
+++ UniCoderApp.py
@@ -0,0 +1,19 @@
+
+a = (
+ """
+çakır -> cakir
+
+"""   
+"""
+merhaba bu kodun amacı yazıyı türkce karakterlerden arındırmaktır.
+kodu çalıştırıp metinleri karşılaştırabilirsiniz
+"""
+)
+
+a = a.replace('ç','c').replace('ş','s').replace('ı','i').replace('ğ','g').replace('ö','o').replace('ü','u').replace('Ç','C').replace('Ş','S').replace('İ','I').replace('Ğ','G').replace('Ö','O').replace('Ü','U')
+print(str(a))
+
+
+while True:
+    pass
+
