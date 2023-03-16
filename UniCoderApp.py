69f1d39fa992a5c0cd878a1d598ac2dfec1bf2d3
Mert Çakır <82875825+MERT-CKR@users.noreply.github.com>
2023-03-16 21:21:59 +0300

    2.0

--- UniCoderApp.py
+++ UniCoderApp.py
@@ -1,14 +1,13 @@
-
+print('bu kodun amacı yazıyı türkçe karakterlerden arındırmaktır.\nkodu çalıştırıp metinleri türkçeye özgü harflerden arındırabilirsiniz.\nörn: Türkçe -> Turkce')
 a = (
  """
-çakır -> cakir
-
 """   
 """
-merhaba bu kodun amacı yazıyı türkce karakterlerden arındırmaktır.
+bu kodun amacı yazıyı türkce karakterlerden arındırmaktır.
 kodu çalıştırıp metinleri karşılaştırabilirsiniz
 """
 )
+a = input("metni giriniz: ")
 
 a = a.replace('ç','c').replace('ş','s').replace('ı','i').replace('ğ','g').replace('ö','o').replace('ü','u').replace('Ç','C').replace('Ş','S').replace('İ','I').replace('Ğ','G').replace('Ö','O').replace('Ü','U')
 print(str(a))
